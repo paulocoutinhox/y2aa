549c9aa0c25c1236a294eaceff4f0df05746b986
Paulo Coutinho <paulo@prsolucoes.com>
2020-09-13 03:39:08 -0300

    removed user component from common

--- common/config/test.php
+++ common/config/test.php
@@ -3,10 +3,4 @@
 return [
     'id' => 'app-common-tests',
     'basePath' => dirname(__DIR__),
-    'components' => [
-        'user' => [
-            'class' => 'yii\web\User',
-            'identityClass' => 'common\models\domain\User',
-        ],
-    ],
 ];
